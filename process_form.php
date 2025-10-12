4b89fb0f1a8e561d76e2c7b46025ea65be8d8290
pyakurelkundan <pyakurelkundan@gmail.com>
2025-10-12 09:32:00 -0400

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,18 +1,5 @@
-
 <?php
-if (isset($_POST['fname'])) {
-    $fname = $_POST['fname'];
-} else {
-    $fname = '';
-}
-
+$fname = $_POST['fname'] ?? '';
 $email = $_POST['email'] ?? '';
-
 echo "Name is: " . htmlspecialchars($fname);
 ?>
-
-
-
-
-
-
